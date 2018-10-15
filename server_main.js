80a563048101c07bb761a2561859699f068f1014
jake5146 <windrose93@hanmail.net>
2018-10-15 17:02:48 -0400

    correct error in server_main.js

--- server_main.js
+++ server_main.js
@@ -76,10 +76,6 @@ function emailLoginPostHandler(rows, req, res, callback) {
 		// 	});
 		// }
 		//@@@@@@@@@@@@@@TEMPORARY CODE @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-
-		//~~~~~~~~~~~~~~~~~ORIGNAL CODE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
-		//compare hashed password and given password using bcrypt
 		bcrypt.compare(req.body[0].password, rows[0].password, function(bErr, response) {
 			//if bcrypt causes error, send error msg.
 			if (bErr) {
@@ -89,16 +85,39 @@ function emailLoginPostHandler(rows, req, res, callback) {
 				sess = req.session;
 				sess.email = req.body[0].email;
 				sess.uid = rows[0].uid;
-				sess.first_name = rows[0].first_name.toLowerCase().replace(/\s+/, "");;
+				sess.first_name = rows[0].first_name;
 				jsonObj.loginCode = 1; //login succeeds
-				jsonObj.first_name = sess.first_name;
-				jsonObj.uid = sess.uid;
+				jsonObj.first_name = req.body[0].first_name;
+				jsonObj.uid = req.body[0].uid;
 			//password mismatched -> login fails
 			} else {
 				jsonObj.loginCode = 0; //login fails
 			}
 			res.end(JSON.stringify(jsonObj));
 		});
+
+
+		//~~~~~~~~~~~~~~~~~ORIGNAL CODE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
+		//compare hashed password and given password using bcrypt
+		// bcrypt.compare(req.body[0].password, rows[0].password, function(bErr, response) {
+		// 	//if bcrypt causes error, send error msg.
+		// 	if (bErr) {
+		// 		jsonObj.msg = bErr;
+  //           //password matched -> login success
+		// 	} else if (response) {
+		// 		sess = req.session;
+		// 		sess.email = req.body[0].email;
+		// 		sess.uid = rows[0].uid;
+		// 		sess.first_name = rows[0].first_name.toLowerCase().replace(/\s+/, "");;
+		// 		jsonObj.loginCode = 1; //login succeeds
+		// 		jsonObj.first_name = sess.first_name;
+		// 		jsonObj.uid = sess.uid;
+		// 	//password mismatched -> login fails
+		// 	} else {
+		// 		jsonObj.loginCode = 0; //login fails
+		// 	}
+		// 	res.end(JSON.stringify(jsonObj));
+		// });
 		//~~~~~~~~~~~~~~~~~ORIGNAL CODE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
 	}
 }
