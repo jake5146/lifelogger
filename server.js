4c28823a47d33dbca428869a1614ec253fa192c8
jake5146 <windrose93@hanmail.net>
2018-10-16 16:51:37 -0400

    try match origin to true

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ var PORT = process.env.PORT || 3100;
 server.listen(PORT);
 var io 			= 	require("socket.io")(server);
 
+io.configure(function() {
+	io.set("match origin protocol", true);
+});
+
 var storage 	= 	multer.diskStorage({
 	destination: "./views/pages/uploads/",
 	filename: function(req, file, callback) {
